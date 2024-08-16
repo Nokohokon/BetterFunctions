16c6ae51715fdf4f55491993067b500bfe2fc2f2
Konja Rehm <111249694+Nokohokon@users.noreply.github.com>
2024-08-16 16:59:23 +0200

    adding nice functions!

--- src/betterfunctions/functions.py
+++ src/betterfunctions/functions.py
@@ -2,10 +2,12 @@ class Functions:
 
 
     def intput(self,text: str = None):
-        inp = input(str)
+        inp = input(text)
         try:
             x = int(inp)
             return x
         except Exception as e:
             raise("Please give a number for input.")
 
+    def output(self,value):
+        print(value)
